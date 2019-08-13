0e88ecfa029e891ba761869f77497026468fd848
Brad Rice <bradrice1@gmail.com>
2019-08-13 08:53:58 -0400

    basic layout created

--- home/models.py
+++ home/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    pass
+    """Home page model."""
+
+    template = "home/home_page.html"
+    max_count = 1
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    left_col = RichTextField()
+    right_col = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title"),
+        FieldPanel("left_col"),
+        FieldPanel("right_col")
+    ]
+
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
